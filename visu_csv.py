77284c5930dc6eeb1bf611dd12268b4a516c20cd
tbayetird <theophile.bayet@ird.fr>
2019-09-20 11:33:05 +0000

    visualisation code

---
+++ visu_csv.py
@@ -0,0 +1,12 @@
+from utils import wavvisualisation as wv
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--path", required=False,
+	help="path to the csv containing the datas we want to visualize ",
+    default ='D:\\datas\\SON\\OUAKAM_AVRIL_MAI_2018\\2018-05\\prepared_dataset\\0.features\\labels.csv' )
+args = vars(ap.parse_args())
+feature_dir = args['path']
+
+
+wv.visualize_all_from_labels(args['path'])
